936ece05a18511d97e8ce84e2eb16642fdd53693
rishik-karthik <venurishik@gmail.com>
2025-10-02 20:54:12 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const darkMode = 'class';
+export const content = [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust to your folder structure
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
